07e05acc00d55b0a0dbbf89b09cf027091654c18
viveklele <viveklele123@gmail.com>
2024-09-19 20:27:03 +0530

    bot running sucessfully

--- commands/utility/user.js
+++ commands/utility/user.js
@@ -3,7 +3,7 @@ const {SlashCommandBuilder} = require('discord.js');
 module.exports = {
     data : new SlashCommandBuilder()
         .setName('user')
-        .description('provide info about user'),
+        .setDescription('provide info about user'),
         async execute(interaction){
             await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`)
         },
